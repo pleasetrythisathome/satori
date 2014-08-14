c911f545160c3955c9c947c44e349aa296131882
Dylan Butman <dbutman@nyu.edu>
2014-08-14 18:15:48 -0400

    doc strings and refactoring

--- src/clj/satori/templater.clj
+++ src/clj/satori/templater.clj
@@ -6,64 +6,84 @@
             [clojure.walk :as w])
   (:use [cfg.current]))
 
-(defn read-all
-  [filename]
-  (let [r (java.io.PushbackReader.
-           (clojure.java.io/reader filename))]
-    (let [eof (Object.)]
-      (take-while #(not= % eof) (repeatedly #(read r false eof))))))
-
-(defn not-gitignored? [file]
-  (str/blank? (->> file
-                   .getAbsolutePath
-                   (sh "git" "check-ignore")
-                   :out)))
-
-(defn get-project-files []
-  (->> @project
-       :root
-       io/file
-       file-seq
-       (filter not-gitignored?)
-       (filter #(.isFile %))))
-
-(defn replace-project-name [file name]
-  (-> file
-      .getAbsolutePath
-      slurp
-      (str/replace name "{{name}}")))
+;; ===== general utils =====
 
 (defn sanitize
   "Replace hyphens with underscores."
   [s]
   (str/replace s "-" "_"))
 
-(defn get-render-path [file root name]
-  (-> file
-      .getAbsolutePath
-      (str/replace root "")
-      (str/replace (sanitize name) "{{sanitized}}")))
+(defn symbol-first?
+  "returns true if sym is the first item in seq"
+  [sym seq]
+  (and (seq? seq)
+       (-> seq first
+           (= sym))))
 
-(defn get-file-name [file]
-  (let [name (.getName file)]
-    (if (= \. (first name))
-      (subs name 1)
-      name)))
+;; ===== file io =====
 
-(defn spit-forms [path forms]
+(defn read-all
+  "reads all lines from a file and returns a sequence of forms"
+  [path]
+  (let [r (java.io.PushbackReader.
+           (clojure.java.io/reader path))]
+    (let [eof (Object.)]
+      (take-while #(not= % eof) (repeatedly #(read r false eof))))))
+
+(defn spit-seq
+  "spits all the items in a seq concatted into a file"
+  [path forms]
   (doall (map-indexed (fn [idx form]
                         (spit path (pr-str form) :append (not (zero? idx))))
                       forms)))
 
+(defn gitignored?
+  "returns true if the file would be ignored by git"
+  [file]
+  (not (str/blank? (->> file
+                        .getAbsolutePath
+                        (sh "git" "check-ignore")
+                        :out))))
+
+(defn unhide
+  "removes a prefixed . if it exists"
+  [filename]
+  (cond-> filename
+          (= \. (first filename)) (subs 1)))
+
+(defn get-project-files
+  "returns all files in the project that are not ignored by git"
+  []
+  (->> @project
+       :root
+       io/file
+       file-seq
+       (filter (complement gitignored?))
+       (filter #(.isFile %))))
+
+;; ===== lein utils =====
+
+(defn replace-template-var
+  "replace a matched string with a lein template variable"
+  [s match var]
+  (str/replace s match (str "{{" var "}}")))
+
+(defn fresh-template [title dir]
+  (sh "lein" "new" "template" title "--to-dir" dir)
+  (sh "rm" (str dir "/src/leiningen/new/" title "/foo.clj")))
+
+;; ===== file processors =====
+
 (defn build-renderers [files root name]
-  (map (juxt #(get-render-path % root name)
-             #(seq ['render (get-file-name %) 'data]))
+  (map (juxt (fn [file]
+               (-> file
+                   .getAbsolutePath
+                   (str/replace root "")
+                   (replace-template-var (sanitize name) "sanitized")))
+             (fn [file]
+               (seq ['render (unhide (.getName file)) 'data])))
        files))
 
-(defn symbol-first? [sym seq]
-  (and (seq? seq)
-       (-> seq first
-           (= sym))))
 
 (defn create-template []
   (let [{:keys [name root template]} @project
