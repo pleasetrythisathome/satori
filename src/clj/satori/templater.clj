1fb24173e5fd64163a288080d33113055d26da39
Dylan Butman <dbutman@nyu.edu>
2014-08-14 19:42:19 -0400

    file overrides

--- src/clj/satori/templater.clj
+++ src/clj/satori/templater.clj
@@ -51,15 +51,29 @@
   (cond-> filename
           (= \. (first filename)) (subs 1)))
 
+(defn get-rel-path [file]
+  (-> file
+      .getAbsolutePath
+      (str/replace (str (:root @project) "/") "")))
+
+(defn is-overridden?
+  "takes either keys or vals from file-overrides and returns a function that
+returns true if the file has an override path defined in the template project settings"
+  [f]
+  (fn [file]
+    (let [{:keys [root template]} @project]
+      (contains? (set (f (:file-overrides template))) (get-rel-path file)))))
+
 (defn get-project-files
   "returns all files in the project that are not ignored by git"
   []
-  (->> @project
-       :root
-       io/file
-       file-seq
-       (filter (complement gitignored?))
-       (filter #(.isFile %))))
+  (let [{:keys [root template]} @project]
+    (->> root
+         io/file
+         file-seq
+         (filter (complement gitignored?))
+         (filter (complement (is-overridden? vals)))
+         (filter #(.isFile %)))))
 
 ;; ===== lein utils =====
 
@@ -69,6 +83,7 @@
   (str/replace s match (str "{{" var "}}")))
 
 (defn fresh-template [title dir]
+  (sh "rm" "-rf" dir)
   (sh "lein" "new" "template" title "--to-dir" dir)
   (sh "rm" (str dir "/src/leiningen/new/" title "/foo.clj")))
 
@@ -84,13 +99,19 @@
                (seq ['render (unhide (.getName file)) 'data])))
        files))
 
-(defmulti process-file
-  (fn [file]
-    (.getName file)))
-(defmethod process-file :default [file project])
+(defmulti process-file (is-overridden? keys))
 
-(defn create-renderer-visitor [{:keys [name root] :as project}]
-  (fn [form]
+(defmethod process-file false [file]
+  (-> file
+      .getAbsolutePath
+      slurp
+      (replace-template-var (:name @project) "name")))
+
+(defmethod process-file true [file]
+  (process-file (io/file (get-in @project [:template :file-overrides (get-rel-path file)]))))
+
+(defn create-renderer-visitor [form]
+  (let [{:keys [name root] :as project} @project]
     (cond
      (symbol-first? '->files form) (concat (butlast form) (build-renderers (get-project-files) root name))
      (symbol-first? 'main/info form) (concat (butlast form) [(get-in project [:template :msg])])
@@ -100,7 +121,7 @@
 
 (defn create-template []
   (let [{:keys [name root template] :as proj} @project
-        {:keys [output-dir title project msg]} template
+        {:keys [output-dir title]} template
 
         target-dir (str root "/" output-dir)
         lein-dir (str target-dir "/src/leiningen/new/")
@@ -109,14 +130,9 @@
 
     (fresh-template title target-dir)
 
-    (spit-seq renderer-path (w/postwalk (create-renderer-visitor proj) (read-all renderer-path)))
+    (spit-seq renderer-path (w/postwalk create-renderer-visitor (read-all renderer-path)))
 
     (doseq [file (get-project-files)]
-      (spit (str src-dir (unhide (.getName file))) (-> file
-                                                       .getAbsolutePath
-                                                       slurp
-                                                       (replace-template-var name "name"))))
-    )
-  )
+      (spit (str src-dir (unhide (.getName file))) (process-file file)))))
 
-(create-template)
+;; (create-template)
