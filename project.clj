db00e306c051e3a637defea15b73ff86074488f9
Dylan Butman <dbutman@nyu.edu>
2014-08-19 11:55:59 -0400

    tempater settings

--- project.clj
+++ project.clj
@@ -11,6 +11,7 @@
                  [org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2288"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
+                 [org.clojure/core.match "0.2.1"]
 
                  ;; server
                  [http-kit "2.1.18"]
@@ -24,13 +25,13 @@
                  [om "0.7.1"]
                  [sablono "0.2.21"]
                  [ankha "0.1.3"]
-                 [shodan "0.3.0"]
 
                  [rewrite-clj "0.3.9"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
             [slothcfg "1.0.1"]
-            [lein-plz "0.1.1"]]
+            [lein-plz "0.1.1"]
+            [lein-templater "0.1.0-SNAPSHOT"]]
 
   :hooks [leiningen.cljsbuild]
 
@@ -40,7 +41,9 @@
 
   :template {:output-dir "lein-template"
              :title "satori"
+             :version "0.1.0"
              :project {:description "A leiningen template for Satori"
                        :url "http://www.github.com/"}
-             :msg "There is no beginning or ending just change"
+             :readme "resources/README.template.md"
+             :msg "There is no beginning or ending, just change."
              :file-overrides {"README.md" "resources/README.template.md"}})
